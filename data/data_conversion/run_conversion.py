173a990c33c5510da541e415efd65a45e1d70947
jonathanwvd <jonathanwvd@gmail.com>
2020-04-18 08:24:30 -0300

    small chagnes

--- data/data_conversion/run_conversion.py
+++ data/data_conversion/run_conversion.py
@@ -6,13 +6,13 @@ Run csv2hdf function to generate the HDF5 file.
 from csv2hdf import csv_to_hdf, new_dataset
 
 # jelali_huang dataset
-# path = 'jelali_huang_to_csv/jelali_huang'
-# name = 'jelali_huang'
-# read_only = True
-# csv_to_hdf(path, name, read_only=read_only)
+path = 'jelali_huang_to_csv/jelali_huang'
+name = 'jelali_huang'
+read_only = True
+csv_to_hdf(path, name, read_only=read_only)
 
 
 # # saved_data dataset
-path = ''
-name = 'saved_data'
-new_dataset(path, name)
+# path = ''
+# name = 'saved_data'
+# new_dataset(path, name)
